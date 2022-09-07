f9b7db6d662fd204c89b583db6b854806e2db2c2
waheedHnblaas <waheed.hanblaas@gmail.com>
2022-09-07 21:26:47 +0300

    add json images

--- lib/view/widget/auth/apploginlogo.dart
+++ lib/view/widget/auth/apploginlogo.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
 class AppLoginLogo extends StatelessWidget {
   const AppLoginLogo({Key? key, required this.name}) : super(key: key);
 
   final String name;
   @override
   Widget build(BuildContext context) {
-    return Image.asset(name,width: Get.width/2,fit: BoxFit.cover,);
+    return Lottie.asset(name);
   }
 }
